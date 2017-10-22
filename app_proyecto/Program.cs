20392e3ea09cccdbfdaadc0d34a398fa5f41e9f3
AlvaroToscano <alvaro5597lt@gmail.com>
2017-10-22 23:39:16 +0200

    Menu's

--- app_proyecto/Program.cs
+++ app_proyecto/Program.cs
@@ -16,7 +16,7 @@ namespace app_proyecto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new MenuCajera());
         }
     }
 }
